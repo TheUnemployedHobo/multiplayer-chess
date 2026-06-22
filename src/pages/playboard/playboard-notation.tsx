98e175a3237b686148ca448c6fdbf9e8b54d1a73
The Unemployed Hobo <mohammadmehdibeheshti@yahoo.com>
2026-06-22 23:50:22 +0330

    Add PlayBoardNotation component to display game moves

---
+++ src/pages/playboard/playboard-notation.tsx
@@ -0,0 +1,15 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+
+function PlayBoardNotation() {
+  return (
+    <Card className="grow">
+      <CardHeader>
+        <CardTitle>Moves</CardTitle>
+        <CardDescription>No moves yet.</CardDescription>
+      </CardHeader>
+      <CardContent>Nigga moved to africa</CardContent>
+    </Card>
+  )
+}
+
+export default PlayBoardNotation
