b25f1f11220d51781dc38e465ecf18b3096fbe6b
The Unemployed Hobo <mohammadmehdibeheshti@yahoo.com>
2026-06-22 23:50:03 +0330

    Enhance getCheckStyle function to apply visual indication for check state

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,21 @@
+import type { CSSProperties } from "react"
+
+import { type Chess, SQUARES } from "chess.js"
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs))
+
+export const getCheckStyle = (chess: Chess): Record<string, CSSProperties> => {
+  if (!chess.isCheck()) return {}
+
+  const checkedColor = chess.turn()
+
+  const kingSquare = SQUARES.find(
+    (square) => chess.get(square)?.type === "k" && chess.get(square)?.color === checkedColor,
+  )
+
+  if (!kingSquare) return {}
+
+  return { [kingSquare]: { background: "rgba(255, 0, 0, 0.5)" } }
+}
