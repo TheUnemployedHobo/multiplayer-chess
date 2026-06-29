9239a9dbfc71c300ea245528ad5d9f9fef469a3c
The Unemployed Hobo <mohammadmehdibeheshti@yahoo.com>
2026-06-29 23:52:54 +0330

    Refactor App component to implement routing for entrance pages

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,26 @@
+import { Route, Switch } from "wouter"
+
 import { ThemeProvider } from "@/components/theme-provider"
 
-// import DashboardPage from "./pages/dashboard"
-import PlayBoardPage from "./pages/playboard"
+import EntranceAvatarChoosingPage from "./pages/entrance/entrance-avatar-choosing"
+import EntranceEnterPage from "./pages/entrance/entrance-enter"
+import EntranceRegisterPage from "./pages/entrance/entrance-register"
+
+const routes = [
+  { component: EntranceRegisterPage, path: "/entrance/register" },
+  { component: EntranceAvatarChoosingPage, path: "/entrance/avatar-choosing" },
+  { component: EntranceEnterPage, path: "/entrance/enter" },
+  { component: () => <>404: Page not found</>, path: undefined },
+]
 
 export default function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <PlayBoardPage />
+      <Switch>
+        {routes.map(({ component, path }) => (
+          <Route key={path} {...{ component, path }} />
+        ))}
+      </Switch>
     </ThemeProvider>
   )
 }
