ee519d62b9414b5abbd309115787159ddfd8a17f
The Unemployed Hobo <mohammadmehdibeheshti@yahoo.com>
2026-06-30 19:56:02 +0330

    Implement EntranceEloRatingForm with form structure and styling

--- src/pages/entrance/entrance-elo-rating-form.tsx
+++ src/pages/entrance/entrance-elo-rating-form.tsx
@@ -1,3 +1,29 @@
-export default function EntranceEloRatingForm() {
-  return <div></div>
+import { FormIcon, UserRoundPlusIcon } from "lucide-react"
+
+import SubmitButton from "@/components/submit-button"
+import { Badge } from "@/components/ui/badge"
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+
+import type { EntranceFormPropsType } from "."
+
+export default function EntranceEloRatingForm({ handleAction }: Omit<EntranceFormPropsType, "setPage">) {
+  return (
+    <form action={handleAction} className="flex h-dvh items-center justify-center">
+      <Card className="z-10 mx-5 w-96">
+        <CardHeader>
+          <CardTitle>
+            <Badge className="size-8 rounded-full p-0" variant="secondary">
+              <FormIcon />
+            </Badge>
+            <span className="ml-2">Log In</span>
+          </CardTitle>
+          <CardDescription>Enter your credentials to access your account</CardDescription>
+        </CardHeader>
+        <CardContent className="space-y-5"></CardContent>
+        <CardFooter>
+          <SubmitButton icon={<UserRoundPlusIcon />} text="Register" />
+        </CardFooter>
+      </Card>
+    </form>
+  )
 }
