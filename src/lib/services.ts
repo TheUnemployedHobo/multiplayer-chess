f9a228169dafed65e6e3593830e96432ed1708ae
The Unemployed Hobo <mohammadmehdibeheshti@yahoo.com>
2026-06-30 20:21:15 +0330

    Refactor register and logIn functions to remove error handling and return response status directly

--- src/lib/services.ts
+++ src/lib/services.ts
@@ -1,25 +1,17 @@
 export const register = async (username: string, password: string, avatar: string, elo: number) => {
-  try {
-    const response = await fetch("http://localhost:3000/users/register", {
-      body: JSON.stringify({ avatar, elo, password, username }),
-      headers: { "Content-Type": "application/json" },
-      method: "POST",
-    })
-    return response.ok
-  } catch {
-    return false
-  }
+  const response = await fetch("http://localhost:3000/users/register", {
+    body: JSON.stringify({ avatar, elo, password, username }),
+    headers: { "Content-Type": "application/json" },
+    method: "POST",
+  })
+  return response.status
 }
 
 export const logIn = async (username: string, password: string) => {
-  try {
-    const response = await fetch("http://localhost:3000/users/login", {
-      body: JSON.stringify({ password, username }),
-      headers: { "Content-Type": "application/json" },
-      method: "POST",
-    })
-    return response.text()
-  } catch {
-    return ""
-  }
+  const response = await fetch("http://localhost:3000/users/login", {
+    body: JSON.stringify({ password, username }),
+    headers: { "Content-Type": "application/json" },
+    method: "POST",
+  })
+  return response.text()
 }
