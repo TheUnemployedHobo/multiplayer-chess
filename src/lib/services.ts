6e6a816985043deb50515656f02c19ffe6526f69
The Unemployed Hobo <mohammadmehdibeheshti@yahoo.com>
2026-06-30 18:14:58 +0330

    Add user registration and login services

---
+++ src/lib/services.ts
@@ -0,0 +1,25 @@
+export const register = async (username: string, password: string, avatar: string) => {
+  try {
+    const response = await fetch("http://localhost:3000/users/register", {
+      body: JSON.stringify({ avatar, password, username }),
+      headers: { "Content-Type": "application/json" },
+      method: "POST",
+    })
+    return response.ok
+  } catch {
+    return false
+  }
+}
+
+export const logIn = async (username: string, password: string) => {
+  try {
+    const response = await fetch("http://localhost:3000/users/login", {
+      body: JSON.stringify({ password, username }),
+      headers: { "Content-Type": "application/json" },
+      method: "POST",
+    })
+    return response.text()
+  } catch {
+    return ""
+  }
+}
