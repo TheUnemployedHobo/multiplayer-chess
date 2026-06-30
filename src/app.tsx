93d20fcc825387694c4a4a3cf50d4d36a0c6322e
The Unemployed Hobo <mohammadmehdibeheshti@yahoo.com>
2026-06-30 18:14:26 +0330

    Refactor routing in App component to simplify entrance page handling

--- src/app.tsx
+++ src/app.tsx
@@ -3,21 +3,20 @@ import { Route, Switch } from "wouter"
 import { ThemeProvider } from "@/components/theme-provider"
 
 import { Toaster } from "./components/ui/sonner"
-import EntranceRegisterPage from "./pages/entrance/entrance-register"
-import EntranceSignInPage from "./pages/entrance/entrance-sign-in"
+import DashboardPage from "./pages/dashboard"
+import EntrancePage from "./pages/entrance"
 
 const routes = [
-  { component: EntranceRegisterPage, path: "/entrance/register" },
-  { component: EntranceSignInPage, path: "/entrance/sign-in" },
-  { component: () => <>404: Page not found</>, path: undefined },
+  { component: EntrancePage, path: "/entrance" },
+  { component: DashboardPage, path: "/dashboard" },
 ]
 
 export default function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <Switch>
-        {routes.map(({ component, path }) => (
-          <Route key={path} {...{ component, path }} />
+        {routes.map((route) => (
+          <Route key={route.path} {...route} />
         ))}
       </Switch>
       <Toaster position="bottom-right" />
