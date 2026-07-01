2a5d8a5f280c7af80173241a0235cec21418b189
The Unemployed Hobo <mohammadmehdibeheshti@yahoo.com>
2026-07-01 18:25:01 +0330

    Add AvatarPopover component for avatar selection with toggle functionality

---
+++ src/components/avatar-popover.tsx
@@ -0,0 +1,47 @@
+import { SquareMousePointerIcon } from "lucide-react"
+import { useState } from "react"
+
+import avatars, { type AvatarType, findAvatarByName } from "@/lib/avatars"
+
+import { Avatar, AvatarImage } from "./ui/avatar"
+import { Badge } from "./ui/badge"
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover"
+import { ToggleGroup, ToggleGroupItem } from "./ui/toggle-group"
+
+export default function AvatarPopover() {
+  const [avatar, setAvatar] = useState(avatars.at(0)!)
+
+  return (
+    <>
+      <Popover>
+        <PopoverTrigger>
+          <Avatar className="ring-secondary relative size-24 cursor-pointer ring-4">
+            <AvatarImage alt={avatar.name} className="size-full" src={avatar.svgSrc} />
+            <Badge className="absolute -right-3 -bottom-1" variant="secondary">
+              <SquareMousePointerIcon />
+              <span>Pick</span>
+            </Badge>
+          </Avatar>
+        </PopoverTrigger>
+        <PopoverContent>
+          <ToggleGroup
+            className="flex flex-wrap justify-between"
+            onValueChange={(value: AvatarType["name"]) => value && setAvatar(findAvatarByName(value)!)}
+            type="single"
+            value={avatar.name}
+            variant="outline"
+          >
+            {avatars.map(({ name, svgSrc }) => (
+              <ToggleGroupItem className="size-14 rounded-full p-0" key={name} value={name}>
+                <Avatar size="lg">
+                  <AvatarImage alt={name} src={svgSrc} />
+                </Avatar>
+              </ToggleGroupItem>
+            ))}
+          </ToggleGroup>
+        </PopoverContent>
+      </Popover>
+      <input name="avatar" type="hidden" value={avatar.name} />
+    </>
+  )
+}
