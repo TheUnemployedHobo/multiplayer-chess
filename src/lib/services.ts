9ad12ab887b3cb8301a9ec4c96e54152c23b4f77
The Unemployed Hobo <mohammadmehdibeheshti@yahoo.com>
2026-07-01 17:12:52 +0330

    Refactor register and logIn functions to use BASE_URL constant for improved maintainability

--- src/lib/services.ts
+++ src/lib/services.ts
@@ -1,17 +1,15 @@
-export const register = async (username: string, password: string, avatar: string, elo: number) => {
-  const response = await fetch("http://localhost:3000/users/register", {
+const BASE_URL = "http://localhost:3000"
+
+export const register = async (username: string, password: string, avatar: string, elo: number) =>
+  fetch(`${BASE_URL}/users/register`, {
     body: JSON.stringify({ avatar, elo, password, username }),
     headers: { "Content-Type": "application/json" },
     method: "POST",
   })
-  return response.status
-}
 
-export const logIn = async (username: string, password: string) => {
-  const response = await fetch("http://localhost:3000/users/login", {
+export const logIn = async (username: string, password: string) =>
+  fetch(`${BASE_URL}/users/login`, {
     body: JSON.stringify({ password, username }),
     headers: { "Content-Type": "application/json" },
     method: "POST",
   })
-  return response.text()
-}
