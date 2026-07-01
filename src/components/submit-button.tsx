75c6135650d76c3ed35b2e2965ea8cf10545c6a5
The Unemployed Hobo <mohammadmehdibeheshti@yahoo.com>
2026-07-01 14:25:15 +0330

    Update SubmitButton component to use 'grow' class for consistent styling

--- src/components/submit-button.tsx
+++ src/components/submit-button.tsx
@@ -10,7 +10,7 @@ export default function SubmitButton({ icon, text }: PropsType) {
   const { pending } = useFormStatus()
 
   return (
-    <Button className="w-full" disabled={pending} size="lg" type="submit">
+    <Button className="grow" disabled={pending} size="lg" type="submit">
       {icon}
       <span>{text}</span>
       <Activity mode={pending ? "visible" : "hidden"}>
