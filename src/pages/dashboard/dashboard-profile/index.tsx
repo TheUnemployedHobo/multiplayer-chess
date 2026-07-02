4eb7972c04ccd2b54ac592c96737afbfecd7bb60
The Unemployed Hobo <mohammadmehdibeheshti@yahoo.com>
2026-07-02 23:21:43 +0330

    Refactor DashboardProfile component to fetch user data and display dynamic avatar and username

--- src/pages/dashboard/dashboard-profile/index.tsx
+++ src/pages/dashboard/dashboard-profile/index.tsx
@@ -1,23 +1,37 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Item, ItemActions, ItemContent, ItemDescription, ItemMedia, ItemTitle } from "@/components/ui/item"
+import useFetchFn from "@/hooks/use-fetch-fn"
+import { findAvatarByName } from "@/lib/avatars"
+import { getCurrentUser } from "@/lib/services"
 
 import ProfileEditPopover from "./profile-edit-popover"
+import ProfileLogout from "./profile-logout"
 
 export default function DashboardProfile() {
+  const { data } = useFetchFn(getCurrentUser)
+
+  if (!data) return null
+
   return (
     <Item className="md:col-span-3" variant="muted">
       <ItemMedia>
         <Avatar size="lg">
-          <AvatarImage src="https://github.com/evilrabbit.png" />
-          <AvatarFallback>ER</AvatarFallback>
+          <AvatarImage src={findAvatarByName(data.avatar)?.svgSrc} />
+          <AvatarFallback>{data.username.slice(0, 2).toUpperCase()}</AvatarFallback>
         </Avatar>
       </ItemMedia>
       <ItemContent>
-        <ItemTitle>Soy boy lib</ItemTitle>
-        <ItemDescription>Member since Jan 2020</ItemDescription>
+        <ItemTitle>{data.username}</ItemTitle>
+        <ItemDescription>
+          <span>Member since </span>
+          <span>
+            {new Date(data.signup_date).toLocaleDateString("en-US", { day: "numeric", month: "long", year: "numeric" })}
+          </span>
+        </ItemDescription>
       </ItemContent>
       <ItemActions>
         <ProfileEditPopover />
+        <ProfileLogout />
       </ItemActions>
     </Item>
   )
