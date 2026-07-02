18b1640fd0bd9910352739873af9f75ce3fc74d2
The Unemployed Hobo <mohammadmehdibeheshti@yahoo.com>
2026-07-02 16:54:12 +0330

    Refactor services to use environment variable for BASE_URL and improve async handling in getCurrentUser

--- src/lib/services.ts
+++ src/lib/services.ts
@@ -1,15 +1,37 @@
-const BASE_URL = "http://localhost:3000"
+import type { AvatarNameType } from "./avatars"
 
-export const register = async (username: string, password: string, avatar: string, elo: number) =>
+import { jwtCookie } from "./utils"
+
+const BASE_URL = import.meta.env.VITE_BASE_URL
+
+export const register = (username: string, password: string, avatar: string, elo: number) =>
   fetch(`${BASE_URL}/users/register`, {
     body: JSON.stringify({ avatar, elo, password, username }),
     headers: { "Content-Type": "application/json" },
     method: "POST",
   })
 
-export const logIn = async (username: string, password: string) =>
+export const logIn = (username: string, password: string) =>
   fetch(`${BASE_URL}/users/login`, {
     body: JSON.stringify({ password, username }),
     headers: { "Content-Type": "application/json" },
     method: "POST",
   })
+
+export const getCurrentUser = async () => {
+  const jwt = jwtCookie.get()
+  if (!jwt) return null
+
+  const response = await fetch(`${BASE_URL}/users`, { headers: { authorization: jwt } })
+  if (response.status === 401) {
+    jwtCookie.remove()
+    return null
+  }
+
+  return response.json() as Promise<{
+    avatar: AvatarNameType
+    signup_date: string
+    stats: { elo: number; games: number; losses: number; wins: number }
+    username: string
+  }>
+}
