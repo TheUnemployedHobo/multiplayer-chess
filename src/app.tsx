d55f2323e087f0cc98903cc76a4a283312d3f8e1
The Unemployed Hobo <mohammadmehdibeheshti@yahoo.com>
2026-07-02 14:21:22 +0330

    Implement route protection and redirection for authenticated users

--- src/app.tsx
+++ src/app.tsx
@@ -1,23 +1,28 @@
-import { Route, Switch } from "wouter"
+import { Redirect, Route, Switch } from "wouter"
 
 import { ThemeProvider } from "@/components/theme-provider"
 
+import { ProtectedRoute, PublicOnlyRoute } from "./components/route-protection"
 import { Toaster } from "./components/ui/sonner"
+import { isAuthenticated } from "./lib/utils"
 import DashboardPage from "./pages/dashboard"
 import EntrancePage from "./pages/entrance"
 
 const routes = [
-  { component: EntrancePage, path: "/entrance" },
-  { component: DashboardPage, path: "/dashboard" },
+  { component: () => <PublicOnlyRoute component={EntrancePage} />, path: "/entrance" },
+  { component: () => <ProtectedRoute component={DashboardPage} />, path: "/dashboard" },
 ]
 
 export default function App() {
+  const loggedIn = isAuthenticated()
+
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <Switch>
         {routes.map((route) => (
           <Route key={route.path} {...route} />
         ))}
+        <Route component={() => <Redirect replace to={loggedIn ? "/dashboard" : "/entrance"} />} />
       </Switch>
       <Toaster position="bottom-right" />
     </ThemeProvider>
