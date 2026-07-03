76e2016f1c2b3acfe146c7f523f7aef17f269091
The Unemployed Hobo <mohammadmehdibeheshti@yahoo.com>
2026-07-03 15:36:08 +0330

    Add obfuscation plugin configuration for production builds

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,29 @@ import tailwindcss from "@tailwindcss/vite"
 import react, { reactCompilerPreset } from "@vitejs/plugin-react"
 import path from "node:path"
 import { defineConfig } from "vite"
+import obfuscationPlugin from "vite-plugin-javascript-obfuscator"
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss(), babel({ presets: [reactCompilerPreset()] })],
+export default defineConfig(({ mode }) => ({
+  build: { sourcemap: false },
+  plugins: [
+    react(),
+    tailwindcss(),
+    babel({ presets: [reactCompilerPreset()] }),
+    mode === "production" &&
+      obfuscationPlugin({
+        apply: "build",
+        exclude: [/node_modules/],
+        include: ["src/**/*.js", "src/**/*.ts", "src/**/*.jsx", "src/**/*.tsx"],
+        options: {
+          compact: true,
+          controlFlowFlattening: true,
+          deadCodeInjection: true,
+          debugProtection: true,
+          selfDefending: true,
+          stringArray: true,
+          stringArrayThreshold: 0.75,
+        },
+      }),
+  ].filter(Boolean),
   resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
-})
+}))
