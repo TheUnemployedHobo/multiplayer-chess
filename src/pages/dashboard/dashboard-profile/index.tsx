7aeef50106e6a97fc855fe328a482cea5d245ed0
The Unemployed Hobo <mohammadmehdibeheshti@yahoo.com>
2026-07-03 18:11:30 +0330

    Refactor DashboardProfile to use useAuthStore for user data retrieval

--- src/pages/dashboard/dashboard-profile/index.tsx
+++ src/pages/dashboard/dashboard-profile/index.tsx
@@ -1,31 +1,30 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Item, ItemActions, ItemContent, ItemDescription, ItemMedia, ItemTitle } from "@/components/ui/item"
-import useFetchFn from "@/hooks/use-fetch-fn"
+import useAuthStore from "@/hooks/use-auth-store"
 import { findAvatarByName } from "@/lib/avatars"
-import { getCurrentUser } from "@/lib/services"
 
 import ProfileEditPopover from "./profile-edit-popover"
 import ProfileLogout from "./profile-logout"
 
 export default function DashboardProfile() {
-  const { data } = useFetchFn(getCurrentUser)
+  const { user } = useAuthStore()
 
-  if (!data) return null
+  if (!user) return null
 
   return (
     <Item className="md:col-span-3" variant="muted">
       <ItemMedia>
         <Avatar size="lg">
-          <AvatarImage src={findAvatarByName(data.avatar)?.svgSrc} />
-          <AvatarFallback>{data.username.slice(0, 2).toUpperCase()}</AvatarFallback>
+          <AvatarImage src={findAvatarByName(user.avatar)?.svgSrc} />
+          <AvatarFallback>{user.username.slice(0, 2).toUpperCase()}</AvatarFallback>
         </Avatar>
       </ItemMedia>
       <ItemContent>
-        <ItemTitle>{data.username}</ItemTitle>
+        <ItemTitle>{user.username}</ItemTitle>
         <ItemDescription>
           <span>Member since </span>
           <span>
-            {new Date(data.signup_date).toLocaleDateString("en-US", { day: "numeric", month: "long", year: "numeric" })}
+            {new Date(user.signup_date).toLocaleDateString("en-US", { day: "numeric", month: "long", year: "numeric" })}
           </span>
         </ItemDescription>
       </ItemContent>
