29a02cff1362a33728c9b6225dbd880f1c721729
The Unemployed Hobo <mohammadmehdibeheshti@yahoo.com>
2026-07-04 14:33:40 +0330

    Refactor EntranceLogInForm to integrate login logic and enhance user feedback with toast notifications

--- src/pages/entrance/entrance-login-form.tsx
+++ src/pages/entrance/entrance-login-form.tsx
@@ -1,4 +1,6 @@
 import { FormIcon, LogInIcon } from "lucide-react"
+import { toast } from "sonner"
+import { useLocation } from "wouter"
 
 import SubmitButton from "@/components/submit-button"
 import { Badge } from "@/components/ui/badge"
@@ -6,10 +8,30 @@ import { Button } from "@/components/ui/button"
 import { Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import useAuthStore from "@/hooks/use-auth-store"
+import useEntranceStore from "@/hooks/use-entrance-store"
+import { logIn, type UserType } from "@/lib/services"
 
-import type { EntranceFormPropsType } from "."
+export default function EntranceLogInForm() {
+  const { authenticate } = useAuthStore()
+  const { setPage } = useEntranceStore()
+  const [, setLocation] = useLocation()
 
-export default function EntranceLogInForm({ handleAction, setPage }: EntranceFormPropsType) {
+  const handleAction = async (formData: FormData) => {
+    const username = formData.get("username") as string
+    const password = formData.get("password") as string
+
+    const response = await logIn(username, password)
+
+    if (response.status === 200) {
+      const user: UserType = await response.json()
+      authenticate(user)
+      setLocation("/dashboard")
+      toast.success("Login successful", { description: "Welcome back!" })
+    } else if (response.status === 401) toast.error("Login failed", { description: "Invalid username or password." })
+    else toast.error("Login failed", { description: "An error occurred during login. Please try again." })
+  }
+
   return (
     <form action={handleAction} className="flex h-dvh items-center justify-center">
       <Card className="z-10 mx-5 w-96">
