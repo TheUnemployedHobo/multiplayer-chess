e3da053ec94996a45d9ed5a1199a9e8400e4edf2
The Unemployed Hobo <mohammadmehdibeheshti@yahoo.com>
2026-07-07 20:24:15 +0330

    Update API endpoint references to use SERVER_URL instead of BASE_URL

--- src/lib/services.ts
+++ src/lib/services.ts
@@ -14,40 +14,40 @@ export type UserType = {
 
 type StatsType = { elo: number; games: number; losses: number; wins: number }
 
-const BASE_URL = import.meta.env["VITE_BASE_URL"]
+const SERVER_URL = import.meta.env["VITE_SERVER_URL"]
 
 export const register = (username: string, password: string, avatar: string, elo: number) =>
-  fetch(`${BASE_URL}/users/register`, {
+  fetch(`${SERVER_URL}/users/register`, {
     body: JSON.stringify({ avatar, elo, password, username }),
     headers: { "Content-Type": "application/json" },
     method: "POST",
   })
 
 export const logIn = (username: string, password: string) =>
-  fetch(`${BASE_URL}/users/login`, {
+  fetch(`${SERVER_URL}/users/login`, {
     body: JSON.stringify({ password, username }),
     headers: { "Content-Type": "application/json" },
     method: "POST",
   })
 
 export const updateUserInfo = (username: string, password: string, avatar: AvatarNameType) =>
-  authFetch(`${BASE_URL}/users`, {
+  authFetch(`${SERVER_URL}/users`, {
     body: JSON.stringify({ avatar, password, username }),
     headers: { "Content-Type": "application/json" },
     method: "PUT",
   })
 
-export const deleteUser = () => authFetch(`${BASE_URL}/users`, { method: "DELETE" })
+export const deleteUser = () => authFetch(`${SERVER_URL}/users`, { method: "DELETE" })
 
 export const getCurrentUser = async () => {
-  const response = await authFetch(`${BASE_URL}/users`)
+  const response = await authFetch(`${SERVER_URL}/users`)
   if (!response) return null
 
   return response.json() as Promise<UserType>
 }
 
 export const getFriends = async () => {
-  const response = await authFetch(`${BASE_URL}/friends`)
+  const response = await authFetch(`${SERVER_URL}/friends`)
   if (!response) return null
 
   return response.json() as Promise<FriendType[]>
