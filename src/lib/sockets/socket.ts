20acf4adc277da41f0a33096bb0aa43de67fbbdf
The Unemployed Hobo <mohammadmehdibeheshti@yahoo.com>
2026-07-08 12:26:56 +0330

    Add socket connection management functions and error handling

---
+++ src/lib/sockets/socket.ts
@@ -0,0 +1,30 @@
+import { io } from "socket.io-client"
+
+import useAuthStore from "@/hooks/use-auth-store"
+
+import { jwtCookie } from "../utils"
+
+export const socket = io(import.meta.env["VITE_SERVER_URL"], {
+  autoConnect: false,
+  transports: ["websocket"],
+})
+
+export const connectSocket = () => {
+  const { clear } = useAuthStore.getState()
+  const jwt = jwtCookie.get()
+
+  if (!jwt) {
+    clear()
+    return
+  }
+
+  socket.auth = { jwt }
+  socket.connect()
+}
+
+export const disconnectSocket = () => socket.disconnect()
+
+socket.on("connect_error", (err) => {
+  const { clear } = useAuthStore.getState()
+  if (err.message === "Unauthorized") clear()
+})
