ae4f307e0150bcaf20a0b5381ebba63dd63b947a
TheUnemplyedHobo <freakypumpkineer@gmail.com>
2026-07-10 11:18:45 +0330

    Add socket event hooks for online users and friend requests

---
+++ src/hooks/use-socket-events.ts
@@ -0,0 +1,57 @@
+import { useEffect, useEffectEvent, useState } from "react"
+
+import useAuthStore from "@/hooks/use-auth-store"
+import { socket } from "@/lib/socket"
+
+type UserInfoType = { avatar: string; elo: number; userId: string; username: string }
+
+export const useOnlineUsers = () => {
+  const [onlineCount, setOnlineCount] = useState(0)
+
+  useEffect(() => {
+    socket.on("users:online-count", setOnlineCount)
+
+    return () => {
+      socket.off("users:online-count", setOnlineCount)
+    }
+  }, [])
+
+  return onlineCount
+}
+
+export const useSendFriendRequest = () => {
+  const { user } = useAuthStore()
+
+  return (friendId: string) => {
+    if (!user) return
+
+    socket.emit("friends:incoming-request", {
+      avatar: user.avatar,
+      elo: user.stats.elo,
+      friendId,
+      username: user.username,
+    })
+  }
+}
+
+export const useIncomingFriendRequest = (cb: (userInfo: UserInfoType) => void) => {
+  const listener = useEffectEvent(cb)
+
+  useEffect(() => {
+    socket.on("friends:incoming-request", listener)
+
+    return () => {
+      socket.off("friends:incoming-request", listener)
+    }
+  }, [])
+}
+
+// export const useAcceptFriendRequest = (listener: () => void) => {
+//   useEffect(() => {
+//     socket.on("friends:accept-request", listener)
+
+//     return () => {
+//       socket.off("friends:accept-request", listener)
+//     }
+//   }, [])
+// }
