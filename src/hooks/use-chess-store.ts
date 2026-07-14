9853b0bee139c5a20d450901e12a99a2fdae9afe
TheUnemplyedHobo <freakypumpkineer@gmail.com>
2026-07-14 20:54:16 +0330

    Implement chess game state management with move history and reset functionality

--- src/hooks/use-chess-store.ts
+++ src/hooks/use-chess-store.ts
@@ -1,11 +1,45 @@
-import { Chess } from "chess.js"
+import { Chess, type Move, type Square } from "chess.js"
 import { create } from "zustand/react"
 
 type StoreType = {
   chess: Chess
+  history: Move[]
+  move: (from: Square, to: Square) => boolean
   position: { id: number; position: string }
+  reset: () => void
 }
 
-const useChessStore = create<StoreType>()(() => ({}))
+const chess = new Chess()
+
+const useChessStore = create<StoreType>()((set) => ({
+  chess,
+  history: [],
+  move: (from, to) => {
+    const result = chess.move({ from, to })
+
+    if (!result) return false
+
+    set((state) => ({
+      history: [...state.history, result],
+      position: {
+        id: state.position.id + 1,
+        position: chess.fen(),
+      },
+    }))
+
+    return true
+  },
+  position: { id: 0, position: chess.fen() },
+  reset: () => {
+    chess.reset()
+    set((state) => ({
+      history: [],
+      position: {
+        id: state.position.id + 1,
+        position: chess.fen(),
+      },
+    }))
+  },
+}))
 
 export default useChessStore
