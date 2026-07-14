4b97129caebb5415e2df0c919a3b3073c7007a18
TheUnemplyedHobo <freakypumpkineer@gmail.com>
2026-07-14 20:56:53 +0330

    Refactor PlayBoardNotation to display move history from chess state

--- src/pages/playboard/playboard-notation.tsx
+++ src/pages/playboard/playboard-notation.tsx
@@ -1,13 +1,29 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import useChessStore from "@/hooks/use-chess-store"
 
 export default function PlayBoardNotation() {
+  const history = useChessStore((state) => state.history)
+
   return (
     <Card className="grow">
       <CardHeader>
         <CardTitle>Moves</CardTitle>
-        <CardDescription>No moves yet.</CardDescription>
       </CardHeader>
-      <CardContent>Nothing</CardContent>
+      <CardContent>
+        <ol className="flex flex-wrap gap-3">
+          {history.map((_, i) =>
+            i % 2 !== 0 ? null : (
+              <li className="whitespace-nowrap" key={i}>
+                <span>{Math.floor(i / 2) + 1}. </span>
+                <span>
+                  {history.at(i)?.san}
+                  {history.at(i + 1) ? ` ${history.at(i + 1)?.san}` : ""}
+                </span>
+              </li>
+            ),
+          )}
+        </ol>
+      </CardContent>
     </Card>
   )
 }
