3784354e93e9d275b3225a30215279f2777e1fc4
TheUnemplyedHobo <freakypumpkineer@gmail.com>
2026-07-14 11:42:54 +0330

    Add unfriend functionality and improve friend status updates in DashboardFriendsList

--- src/pages/dashboard/dashboard-friends/dashboard-friends-list.tsx
+++ src/pages/dashboard/dashboard-friends/dashboard-friends-list.tsx
@@ -1,27 +1,33 @@
-import useSWR from "swr"
+import { toast } from "sonner"
+import useSWR, { mutate } from "swr"
 
 import { ItemPlaceholder } from "@/components/placeholders"
 import { ShadcnDialog } from "@/components/shadcn-dialogs"
 import { Button } from "@/components/ui/button"
 import { ItemGroup } from "@/components/ui/item"
 import { UserItem } from "@/components/user-item"
-import { useFriendPresence } from "@/hooks/use-socket-events"
+import { useFriendPresence, useFriendRemoval } from "@/hooks/use-socket-events"
 import { getFriends } from "@/lib/services"
 
 import DashboardFriendsProfile from "./dashboard-friends-profile"
 
 export default function DashboardFriendsList() {
-  const { data, mutate } = useSWR("friends", getFriends)
+  const { data, mutate: mutateFriends } = useSWR("friends", getFriends)
 
   useFriendPresence((friend) => {
     const order = { online: 0, playing: 1 } as const
-    mutate(
+    mutateFriends(
       (friends) =>
         friends
           ?.map((each) => (friend.userId === each.id ? { ...each, status: friend.status } : each))
           .sort((a, b) => (order[a.status as keyof typeof order] ?? 2) - (order[b.status as keyof typeof order] ?? 2)),
       false,
     )
+  })
+
+  const unfriend = useFriendRemoval(() => {
+    mutateFriends()
+    mutate("users")
   })
 
   return (
@@ -33,7 +39,16 @@ export default function DashboardFriendsList() {
       ) : (
         data.map((props) => (
           <ShadcnDialog
-            content={<DashboardFriendsProfile {...props} />}
+            content={
+              <DashboardFriendsProfile
+                {...props}
+                handleInvite={() => alert("Invited")}
+                handleUnfriend={() => {
+                  unfriend(props.id)
+                  toast.info(`You are no longer friends with ${props.username}`)
+                }}
+              />
+            }
             key={props.id}
             triggerButton={
               <Button className="h-max w-full p-0" variant="ghost">
