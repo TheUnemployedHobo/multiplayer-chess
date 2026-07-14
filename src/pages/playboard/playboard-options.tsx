24c8dcd1d06f488c29666c0428136dd410cecf77
TheUnemplyedHobo <freakypumpkineer@gmail.com>
2026-07-14 18:08:18 +0330

    Refactor PlayBoardOptions component by removing unused icons and simplifying button layout

--- src/pages/playboard/playboard-options.tsx
+++ src/pages/playboard/playboard-options.tsx
@@ -1,9 +1,8 @@
-import { ClockPlusIcon, FlagIcon, HandshakeIcon, RotateCwSquareIcon, Undo2Icon } from "lucide-react"
+import { FlagIcon, HandshakeIcon } from "lucide-react"
 
 import { Badge } from "@/components/ui/badge"
 import { Button } from "@/components/ui/button"
 import { Card, CardAction, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
-import { Separator } from "@/components/ui/separator"
 
 export default function PlayBoardOptions() {
   return (
@@ -14,29 +13,15 @@ export default function PlayBoardOptions() {
           <Badge>White to move</Badge>
         </CardAction>
       </CardHeader>
-      <CardContent className="space-y-3">
-        <div className="flex gap-x-3">
-          <Button className="grow" size="lg" variant="destructive">
-            <FlagIcon />
-            <span>Resign</span>
-          </Button>
-          <Button className="grow" size="lg" variant="secondary">
-            <HandshakeIcon />
-            <span>Offer draw</span>
-          </Button>
-        </div>
-        <Separator />
-        <div className="flex gap-x-3">
-          <Button className="grow" size="lg" variant="outline">
-            <Undo2Icon />
-          </Button>
-          <Button className="grow" size="lg" variant="outline">
-            <RotateCwSquareIcon />
-          </Button>
-          <Button className="grow" size="lg" variant="outline">
-            <ClockPlusIcon />
-          </Button>
-        </div>
+      <CardContent className="flex gap-x-3">
+        <Button className="grow" size="lg" variant="destructive">
+          <FlagIcon />
+          <span>Resign</span>
+        </Button>
+        <Button className="grow" size="lg" variant="secondary">
+          <HandshakeIcon />
+          <span>Offer draw</span>
+        </Button>
       </CardContent>
     </Card>
   )
