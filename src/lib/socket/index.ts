fe0b447e3213a17a28694819ae7341a8da445105
TheUnemplyedHobo <freakypumpkineer@gmail.com>
2026-07-15 20:43:09 +0330

    Add socket management functionality for real-time communication

---
+++ src/lib/socket/index.ts
@@ -0,0 +1,32 @@
+import { io } from "socket.io-client"
+
+import useAuthStore from "@/hooks/use-auth-store"
+
+import { jwtCookie } from "../utils"
+
+export type FnType<T> = (data: T) => Promise<void> | void
+
+export const socket = io(import.meta.env["VITE_SERVER_URL"], {
+  autoConnect: false,
+  transports: ["websocket"],
+})
+
+export const connectSocket = () => {
+  const { clear } = useAuthStore.getState()
+  const jwt = jwtCookie.get()
+
+  if (!jwt) {
+    clear()
+    return
+  }
+
+  socket.auth = { jwt }
+  socket.connect()
+}
+
+export const disconnectSocket = () => socket.disconnect()
+
+socket.on("connect_error", (err) => {
+  const { clear } = useAuthStore.getState()
+  if (err.message === "Unauthorized") clear()
+})
