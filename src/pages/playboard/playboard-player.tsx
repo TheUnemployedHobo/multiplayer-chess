c1cc3e26e0c941dd5ee2e19d6af123f20f8329c1
TheUnemplyedHobo <freakypumpkineer@gmail.com>
2026-07-15 07:45:12 +0330

    Refactor PlayBoardPlayer to use UserItem for improved player display

--- src/pages/playboard/playboard-player.tsx
+++ src/pages/playboard/playboard-player.tsx
@@ -1,28 +1,21 @@
 import { TimerIcon } from "lucide-react"
 
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Badge } from "@/components/ui/badge"
-import { Item, ItemActions, ItemContent, ItemDescription, ItemMedia, ItemTitle } from "@/components/ui/item"
+import { UserItem } from "@/components/user-item"
 
 export default function PlayBoardPlayer() {
   return (
-    <Item variant="muted">
-      <ItemMedia>
-        <Avatar size="lg">
-          <AvatarImage src="https://github.com/evilrabbit.png" />
-          <AvatarFallback>ER</AvatarFallback>
-        </Avatar>
-      </ItemMedia>
-      <ItemContent>
-        <ItemTitle>Evil Rabbit</ItemTitle>
-        <ItemDescription>Elo 2420</ItemDescription>
-      </ItemContent>
-      <ItemActions>
+    <UserItem
+      actions={
         <Badge className="py-3" variant="outline">
           <TimerIcon />
           <span className="text-base font-bold">10:00</span>
         </Badge>
-      </ItemActions>
-    </Item>
+      }
+      avatar="e8wq4inw"
+      description="ELO: 1200"
+      title="Evil Rabbit"
+      variant="muted"
+    />
   )
 }
