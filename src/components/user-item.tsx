50e704c1539746732b896352d0deaccd6fe46f77
TheUnemplyedHobo <freakypumpkineer@gmail.com>
2026-07-17 18:12:09 +0330

    Feature: add UserItemPlaceholder component for loading state representation

--- src/components/user-item.tsx
+++ src/components/user-item.tsx
@@ -5,6 +5,8 @@ import { Item, ItemActions, ItemContent, ItemDescription, ItemMedia, ItemTitle }
 import { findAvatarByName } from "@/lib/avatars"
 import { cn } from "@/lib/utils"
 
+import { Skeleton } from "./ui/skeleton"
+
 type PropsType = ComponentProps<typeof Item> & {
   actions?: ReactNode
   avatar: string
@@ -34,4 +36,16 @@ export function UserItem({ actions, avatar, description, status, title, ...props
       {actions && <ItemActions>{actions}</ItemActions>}
     </Item>
   )
+}
+
+export function UserItemPlaceholder({ quantity }: { quantity: number }) {
+  return Array.from({ length: quantity }, (_, i) => (
+    <div className="flex items-center gap-x-3" key={i}>
+      <Skeleton className="size-14 rounded-full" />
+      <div className="space-y-2">
+        <Skeleton className="h-5 w-25 rounded-full" />
+        <Skeleton className="h-5 w-36 rounded-full" />
+      </div>
+    </div>
+  ))
 }
