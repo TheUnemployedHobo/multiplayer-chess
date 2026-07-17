9ecbd37dd9003751165eadde7df45a821dd64ef5
TheUnemplyedHobo <freakypumpkineer@gmail.com>
2026-07-17 18:14:08 +0330

    Refactor DashboardPlayVsBotButton to simplify game start logic and remove unused state

--- src/pages/dashboard/dashboard-play-vs-bot/dashboard-play-button.tsx
+++ src/pages/dashboard/dashboard-play-vs-bot/dashboard-play-button.tsx
@@ -1,40 +1,30 @@
 import { PlayIcon } from "lucide-react"
-import { useState } from "react"
 import { useLocation } from "wouter"
 
 import { Button } from "@/components/ui/button"
-import { Spinner } from "@/components/ui/spinner"
 import useChessStore from "@/hooks/use-chess-store"
 import { useBotStart } from "@/lib/socket/event-hooks/use-bot-events"
 
 type PropsType = { difficulty: { label: string; skill: number } }
 
 export default function DashboardPlayVsBotButton({ difficulty: { label, skill } }: PropsType) {
-  const [isPending, setIsPending] = useState(false)
   const setGameMode = useChessStore((state) => state.setGameMode)
   const setBotDifficulty = useChessStore((state) => state.setBotDifficulty)
   const reset = useChessStore((state) => state.reset)
+  const setIsPlaying = useChessStore((state) => state.setIsPlaying)
   const [, setLocation] = useLocation()
 
   const startBotGame = useBotStart(() => {
     reset()
+    setIsPlaying(true)
     setGameMode("bot")
     setBotDifficulty(label)
     setLocation("/playboard")
-    setIsPending(false)
   })
 
   return (
-    <Button
-      className="w-full"
-      disabled={isPending}
-      onClick={() => {
-        setIsPending(true)
-        startBotGame(skill)
-      }}
-      size="lg"
-    >
-      {isPending ? <Spinner /> : <PlayIcon />}
+    <Button className="w-full" onClick={() => startBotGame(skill)} size="lg">
+      <PlayIcon />
       <span>Start the game</span>
     </Button>
   )
