761c0f01020dde0834a40e1e5390ab616035a3e9
TheUnemplyedHobo <freakypumpkineer@gmail.com>
2026-07-17 18:16:13 +0330

    Refactor PlayBoardOptions to improve game state handling and UI updates

--- src/pages/playboard/playboard-options.tsx
+++ src/pages/playboard/playboard-options.tsx
@@ -1,35 +1,44 @@
-import { FlagIcon, HandshakeIcon } from "lucide-react"
+import { HandshakeIcon, Undo2Icon } from "lucide-react"
 
 import { Badge } from "@/components/ui/badge"
 import { Button } from "@/components/ui/button"
 import { Card, CardAction, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import useChessStore from "@/hooks/use-chess-store"
 
+import { BackToDashboardButton, ResignButton } from "./playboard-buttons"
+
 export default function PlayBoardOptions() {
   const turn = useChessStore((state) => state.turn)
   const gameMode = useChessStore((state) => state.gameMode)
+  const isPlaying = useChessStore((state) => state.isPlaying)
 
   return (
     <Card>
       <CardHeader>
         <CardTitle>Game</CardTitle>
         <CardAction>
-          <Badge>
-            <span className="capitalize">{turn}</span>
-            <span>to move</span>
-          </Badge>
+          <Badge>{isPlaying ? <span className="capitalize">{turn} to move</span> : <span>Game over</span>}</Badge>
         </CardAction>
       </CardHeader>
       <CardContent className="flex gap-x-3">
-        <Button className="grow" size="lg" variant="destructive">
-          <FlagIcon />
-          <span>Resign</span>
-        </Button>
-        {gameMode === "multiplayer" && (
-          <Button className="grow" size="lg" variant="secondary">
-            <HandshakeIcon />
-            <span>Offer draw</span>
-          </Button>
+        {isPlaying ? (
+          <>
+            {gameMode === "bot" && (
+              <Button className="grow" size="lg" variant="secondary">
+                <Undo2Icon />
+                <span>Undo</span>
+              </Button>
+            )}
+            {gameMode === "multiplayer" && (
+              <Button className="grow" size="lg" variant="secondary">
+                <HandshakeIcon />
+                <span>Offer draw</span>
+              </Button>
+            )}
+            <ResignButton />
+          </>
+        ) : (
+          <BackToDashboardButton />
         )}
       </CardContent>
     </Card>
