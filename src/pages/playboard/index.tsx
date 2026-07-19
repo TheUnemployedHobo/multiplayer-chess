463f4401cea6f30a1f3640ef171ff698543eaf20
TheUnemplyedHobo <freakypumpkineer@gmail.com>
2026-07-19 21:50:55 +0330

    Refactor PlayBoardPage to use updated modal store and improve bot finished handling

--- src/pages/playboard/index.tsx
+++ src/pages/playboard/index.tsx
@@ -4,30 +4,32 @@ import useChessStore from "@/hooks/use-chess-store"
 import { useOnBotFinished } from "@/lib/socket/event-hooks/use-bot-events"
 
 import PlayBoardChat from "./playboard-chat"
-import PlayBoardModal, { usePlayBoardModalSetIsOpen } from "./playboard-modal"
+import PlayBoardModal, { usePlayBoardModalStore } from "./playboard-modal"
 import PlayBoardNotation from "./playboard-notation"
 import PlayBoardOptions from "./playboard-options"
 import PlayBoardPlank from "./playboard-plank"
 
 export default function PlayBoardPage() {
-  const setIsOpen = usePlayBoardModalSetIsOpen()
   const gameMode = useChessStore((state) => state.gameMode)
   const botDifficulty = useChessStore((state) => state.botDifficulty)
   const setIsPlaying = useChessStore((state) => state.setIsPlaying)
   const { avatar, stats, username } = useAuthStore((state) => state.user)!
+  const setters = usePlayBoardModalStore((state) => state.setters)
 
-  useOnBotFinished(() => {
+  useOnBotFinished(({ result, winner }) => {
     setIsPlaying(false)
-    setIsOpen(true)
+    setters.setIsOpen(true)
+    setters.setTitle(winner ? `${winner} wins` : "Draw")
+    setters.setDescription(result)
   })
 
   return (
     <section className="container mx-auto flex min-h-dvh flex-col gap-3 p-3 md:h-dvh md:flex-row">
       <div className="flex min-h-0 flex-1 flex-col gap-3">
         {/* {gameMode === "multiplayer" && <UserItem />} */}
-        {gameMode === "bot" && <UserItem avatar="bot" description={botDifficulty} title="Engine" />}
+        {gameMode === "bot" && <UserItem avatar="bot" description={botDifficulty} title="Engine" variant="muted" />}
         <PlayBoardPlank />
-        <UserItem avatar={avatar} description={`ELO: ${stats.elo}`} title={username} />
+        <UserItem avatar={avatar} description={`ELO: ${stats.elo}`} title={username} variant="muted" />
       </div>
       <div className="flex min-h-0 flex-col gap-y-3 md:w-75">
         <PlayBoardOptions />
