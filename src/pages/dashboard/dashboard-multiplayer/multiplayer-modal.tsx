192833f68d7710e6b4cc427abe63c1d87aa40f94
TheUnemplyedHobo <freakypumpkineer@gmail.com>
2026-07-20 20:58:42 +0330

    Add MultiplayerModal component for matchmaking interface

---
+++ src/pages/dashboard/dashboard-multiplayer/multiplayer-modal.tsx
@@ -0,0 +1,44 @@
+import { CircleXIcon } from "lucide-react"
+import { combine } from "zustand/middleware"
+import { create } from "zustand/react"
+
+import { ShadcnModal } from "@/components/shadcn-dialogs"
+import { Button } from "@/components/ui/button"
+import { Spinner } from "@/components/ui/spinner"
+import { useMatchLeave } from "@/lib/socket/event-hooks/use-match-events"
+
+export const useMultiplayerModalStore = create(
+  combine({ isOpen: false }, (set) => ({
+    setIsOpen: (isOpen: boolean) => set({ isOpen }),
+  })),
+)
+
+export default function MultiplayerModal() {
+  const isOpen = useMultiplayerModalStore((state) => state.isOpen)
+  const setIsOpen = useMultiplayerModalStore((state) => state.setIsOpen)
+
+  const leaveMatchmaking = useMatchLeave(() => setIsOpen(false))
+
+  return (
+    <ShadcnModal
+      content={
+        <section className="flex flex-col items-center gap-y-3">
+          <div className="bg-secondary rounded-full border p-3">
+            <Spinner className="text-muted-foreground size-8" />
+          </div>
+          <h2 className="text-2xl font-semibold">Finding an opponent...</h2>
+          <p className="text-muted-foreground text-center">
+            We're searching for the best available opponent to give you a fair and challenging match. This may take a
+            few moments.
+          </p>
+          <Button className="w-full" onClick={leaveMatchmaking} size="lg" variant="destructive">
+            <CircleXIcon />
+            <span>Leave the matchmaking</span>
+          </Button>
+        </section>
+      }
+      isOpen={isOpen}
+      onClose={leaveMatchmaking}
+    />
+  )
+}
