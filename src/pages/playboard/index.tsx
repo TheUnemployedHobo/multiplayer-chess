5f7db45b79d780e3bc976025330848304374004a
TheUnemplyedHobo <freakypumpkineer@gmail.com>
2026-07-20 20:59:53 +0330

    Refactor PlayBoardPage to correctly display opponent and user information in multiplayer mode

--- src/pages/playboard/index.tsx
+++ src/pages/playboard/index.tsx
@@ -13,7 +13,8 @@ export default function PlayBoardPage() {
   const gameMode = useChessStore((state) => state.gameMode)
   const botDifficulty = useChessStore((state) => state.botDifficulty)
   const setIsPlaying = useChessStore((state) => state.setIsPlaying)
-  const { avatar, stats, username } = useAuthStore((state) => state.user)!
+  const opponent = useChessStore((state) => state.opponentInfo)!
+  const user = useAuthStore((state) => state.user)!
   const setters = usePlayBoardModalStore((state) => state.setters)
 
   useOnBotFinished(({ result, winner }) => {
@@ -26,10 +27,17 @@ export default function PlayBoardPage() {
   return (
     <section className="container mx-auto flex min-h-dvh flex-col gap-3 p-3 md:h-dvh md:flex-row">
       <div className="flex min-h-0 flex-1 flex-col gap-3">
-        {/* {gameMode === "multiplayer" && <UserItem />} */}
+        {gameMode === "multiplayer" && (
+          <UserItem
+            avatar={opponent.avatar}
+            description={`ELO: ${opponent.elo}`}
+            title={opponent.username}
+            variant="muted"
+          />
+        )}
         {gameMode === "bot" && <UserItem avatar="bot" description={botDifficulty} title="Engine" variant="muted" />}
         <PlayBoardPlank />
-        <UserItem avatar={avatar} description={`ELO: ${stats.elo}`} title={username} variant="muted" />
+        <UserItem avatar={user.avatar} description={`ELO: ${user.stats.elo}`} title={user.username} variant="muted" />
       </div>
       <div className="flex min-h-0 flex-col gap-y-3 md:w-75">
         <PlayBoardOptions />
