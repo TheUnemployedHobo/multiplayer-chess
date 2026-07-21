963d6595f59b183a71cc93aa2812a8968c2826be
TheUnemplyedHobo <freakypumpkineer@gmail.com>
2026-07-21 22:02:45 +0330

    Refactor move handling to separate bot and multiplayer logic in PlayBoardPlank

--- src/pages/playboard/playboard-plank.tsx
+++ src/pages/playboard/playboard-plank.tsx
@@ -4,30 +4,46 @@ import { toBoardMoveDestinations, toGameMove } from "@mirasen/react-chessboard/a
 import { Card } from "@/components/ui/card"
 import useChessStore from "@/hooks/use-chess-store"
 import { useBotMove } from "@/lib/socket/event-hooks/use-bot-events"
+import { useGameMove } from "@/lib/socket/event-hooks/use-game-events"
 
 export default function PlayBoardPlank() {
   const chess = useChessStore((state) => state.chess)
   const position = useChessStore((state) => state.position)
   const orientation = useChessStore((state) => state.orientation)
+  const gameMode = useChessStore((state) => state.gameMode)
+  const isPlaying = useChessStore((state) => state.isPlaying)
   const tryMove = useChessStore((state) => state.tryMove)
   const forceMove = useChessStore((state) => state.forceMove)
 
-  const moveBotPieces = useBotMove(({ from, to }) => {
-    forceMove(from, to)
-  })
+  const sendBotMove = useBotMove(({ from, to }) => forceMove(from, to))
+  const sendOpponentMove = useGameMove(({ from, promotion, to }) => forceMove(from, to, promotion))
 
   return (
     <Card className="size-full justify-center p-3">
       <Chessboard
         className="aspect-square size-full"
         movability={{
-          destinations: (source) => toBoardMoveDestinations(chess.moves({ square: source, verbose: true })),
+          destinations: (source) => {
+            if (!isPlaying) return []
+
+            const piece = chess.get(source)
+            if (!piece) return []
+
+            const playerColor = orientation === "white" ? "w" : "b"
+
+            if (piece.color !== playerColor) return []
+            if (piece.color !== chess.turn()) return []
+
+            return toBoardMoveDestinations(chess.moves({ square: source, verbose: true }))
+          },
           mode: "strict",
         }}
         onUIMove={(uiMove) => {
           const gameMove = toGameMove(uiMove)
           if (tryMove(gameMove.from, gameMove.to, gameMove.promotion)) {
-            moveBotPieces({ from: gameMove.from, promotion: gameMove.promotion, to: gameMove.to })
+            if (gameMode === "bot") sendBotMove({ from: gameMove.from, promotion: gameMove.promotion, to: gameMove.to })
+            if (gameMode === "multiplayer")
+              sendOpponentMove({ from: gameMove.from, promotion: gameMove.promotion, to: gameMove.to })
           }
         }}
         orientation={orientation}
