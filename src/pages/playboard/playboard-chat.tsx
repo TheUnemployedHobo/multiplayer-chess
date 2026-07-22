ee08c8d6a6f42458b209fd569728687900077f0f
TheUnemplyedHobo <freakypumpkineer@gmail.com>
2026-07-22 14:51:03 +0330

    Implement dynamic message handling in PlayBoardChat component

--- src/pages/playboard/playboard-chat.tsx
+++ src/pages/playboard/playboard-chat.tsx
@@ -1,12 +1,20 @@
 import { SendHorizontalIcon } from "lucide-react"
+import { useState } from "react"
 
 import { Bubble, BubbleContent } from "@/components/ui/bubble"
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { ScrollArea } from "@/components/ui/scroll-area"
+import useChessStore from "@/hooks/use-chess-store"
+import { useGameChat } from "@/lib/socket/event-hooks/use-game-events"
 
 export default function PlayBoardChat() {
+  const [messages, setMessages] = useState<{ color: "black" | "white"; message: string }[]>([])
+  const orientation = useChessStore((state) => state.orientation)
+
+  const sendMessage = useGameChat((message) => setMessages((p) => [...p, message]))
+
   return (
     <Card className="h-80 md:h-auto md:flex-1">
       <CardHeader>
@@ -15,32 +23,27 @@ export default function PlayBoardChat() {
       <CardContent className="min-h-0">
         <ScrollArea className="h-full">
           <div className="space-y-3">
-            <Bubble variant="tinted">
-              <BubbleContent>Hey there! What&apos;s up?</BubbleContent>
-            </Bubble>
-            <Bubble variant="muted">
-              <BubbleContent>Not much.</BubbleContent>
-            </Bubble>
-            <Bubble variant="tinted">
-              <BubbleContent>Ready for another match?</BubbleContent>
-            </Bubble>
-            <Bubble variant="muted">
-              <BubbleContent>No not really</BubbleContent>
-            </Bubble>
-            <Bubble variant="tinted">
-              <BubbleContent>Why?</BubbleContent>
-            </Bubble>
-            <Bubble variant="muted">
-              <BubbleContent>Enough chess for today</BubbleContent>
-            </Bubble>
+            {messages.map(({ color, message }, i) => (
+              <Bubble key={i} variant={orientation === color ? "tinted" : "secondary"}>
+                <BubbleContent>{message}</BubbleContent>
+              </Bubble>
+            ))}
           </div>
         </ScrollArea>
       </CardContent>
-      <CardFooter className="mt-auto flex shrink-0 gap-x-3">
-        <Input placeholder="Type something..." />
-        <Button size="icon-lg">
-          <SendHorizontalIcon />
-        </Button>
+      <CardFooter className="mt-auto shrink-0">
+        <form
+          action={(e) => {
+            const msg = e.get("msg") as string
+            if (msg.trim()) sendMessage(msg)
+          }}
+          className="flex grow items-center gap-x-3"
+        >
+          <Input maxLength={100} name="msg" placeholder="Type something..." />
+          <Button size="icon-lg" type="submit">
+            <SendHorizontalIcon />
+          </Button>
+        </form>
       </CardFooter>
     </Card>
   )
