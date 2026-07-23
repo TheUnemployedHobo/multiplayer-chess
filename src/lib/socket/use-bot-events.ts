151d1b0a06b74d806ed446bcc1f4a0a1864124d0
TheUnemplyedHobo <freakypumpkineer@gmail.com>
2026-07-23 22:06:31 +0330

    Add bot event hooks for handling bot actions in the game

---
+++ src/lib/socket/use-bot-events.ts
@@ -0,0 +1,73 @@
+import { useEffect, useEffectEvent } from "react"
+
+import type { FnType, GameFinishedPayload, MovePayload } from "../common-types"
+
+import { socket } from "."
+
+export const useBotStart = (fn: FnType<undefined>) => {
+  const listener = useEffectEvent(fn)
+
+  useEffect(() => {
+    socket.on("bot:start", listener)
+
+    return () => {
+      socket.off("bot:start", listener)
+    }
+  }, [])
+
+  return (skill: number) => socket.emit("bot:start", skill)
+}
+
+export const useBotMove = (fn: FnType<MovePayload>) => {
+  const listener = useEffectEvent(fn)
+
+  useEffect(() => {
+    socket.on("bot:move", listener)
+
+    return () => {
+      socket.off("bot:move", listener)
+    }
+  }, [])
+
+  return (movement: MovePayload) => socket.emit("bot:move", movement)
+}
+
+export const useOnBotFinished = (fn: FnType<GameFinishedPayload>) => {
+  const listener = useEffectEvent(fn)
+
+  useEffect(() => {
+    socket.on("bot:finished", listener)
+
+    return () => {
+      socket.off("bot:finished", listener)
+    }
+  }, [])
+}
+
+export const useBotResign = (fn: FnType<GameFinishedPayload>) => {
+  const listener = useEffectEvent(fn)
+
+  useEffect(() => {
+    socket.on("bot:resign", listener)
+
+    return () => {
+      socket.off("bot:resign", listener)
+    }
+  }, [])
+
+  return () => socket.emit("bot:resign", undefined)
+}
+
+export const useBotUndo = (fn: FnType<undefined>) => {
+  const listener = useEffectEvent(fn)
+
+  useEffect(() => {
+    socket.on("bot:undo", listener)
+
+    return () => {
+      socket.off("bot:undo", listener)
+    }
+  }, [])
+
+  return () => socket.emit("bot:undo", undefined)
+}
