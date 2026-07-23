396477133b4500e4367d1dd1cfe039c96a5340a3
TheUnemplyedHobo <freakypumpkineer@gmail.com>
2026-07-23 22:06:49 +0330

    Add game event hooks for managing moves, resignations, draw offers, and chat

---
+++ src/lib/socket/use-game-events.ts
@@ -0,0 +1,101 @@
+import { useEffect, useEffectEvent } from "react"
+
+import type { FnType, GameFinishedPayload, MovePayload } from "../common-types"
+
+import { socket } from "."
+
+export const useGameMove = (fn: FnType<MovePayload>) => {
+  const listener = useEffectEvent(fn)
+
+  useEffect(() => {
+    socket.on("game:move", listener)
+
+    return () => {
+      socket.off("game:move", listener)
+    }
+  }, [])
+
+  return (movement: MovePayload) => socket.emit("game:move", movement)
+}
+
+export const useOnGameFinish = (fn: FnType<GameFinishedPayload>) => {
+  const listener = useEffectEvent(fn)
+
+  useEffect(() => {
+    socket.on("game:finished", listener)
+
+    return () => {
+      socket.off("game:finished", listener)
+    }
+  }, [])
+}
+
+export const useGameResign = (fn: FnType<GameFinishedPayload>) => {
+  const listener = useEffectEvent(fn)
+
+  useEffect(() => {
+    socket.on("game:resign", listener)
+
+    return () => {
+      socket.off("game:resign", listener)
+    }
+  }, [])
+
+  return () => socket.emit("game:resign", undefined)
+}
+
+export const useGameDrawOffer = (fn: FnType<{ message: string; offerRole: "offeree" | "offeror" }>) => {
+  const listener = useEffectEvent(fn)
+
+  useEffect(() => {
+    socket.on("game:draw-offer", listener)
+
+    return () => {
+      socket.off("game:draw-offer", listener)
+    }
+  }, [])
+
+  return () => socket.emit("game:draw-offer", undefined)
+}
+
+export const useGameDrawOfferDecline = (fn: FnType<string>) => {
+  const listener = useEffectEvent(fn)
+
+  useEffect(() => {
+    socket.on("game:draw-offer:decline", listener)
+
+    return () => {
+      socket.off("game:draw-offer:decline", listener)
+    }
+  }, [])
+
+  return () => socket.emit("game:draw-offer:decline", undefined)
+}
+
+export const useGameDrawOfferAccept = (fn: FnType<undefined>) => {
+  const listener = useEffectEvent(fn)
+
+  useEffect(() => {
+    socket.on("game:draw-offer:accept", listener)
+
+    return () => {
+      socket.off("game:draw-offer:accept", listener)
+    }
+  }, [])
+
+  return () => socket.emit("game:draw-offer:accept", undefined)
+}
+
+export const useGameChat = (fn: FnType<{ color: "black" | "white"; message: string }>) => {
+  const listener = useEffectEvent(fn)
+
+  useEffect(() => {
+    socket.on("game:chat", listener)
+
+    return () => {
+      socket.off("game:chat", listener)
+    }
+  }, [])
+
+  return (message: string) => socket.emit("game:chat", message)
+}
