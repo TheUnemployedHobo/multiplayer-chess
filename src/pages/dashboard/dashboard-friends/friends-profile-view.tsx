015eca9a3970d22f73a3923f6ee6db4fa279edbe
TheUnemplyedHobo <freakypumpkineer@gmail.com>
2026-07-23 22:08:03 +0330

    Add FriendsProfileView component for displaying friend details and actions

---
+++ src/pages/dashboard/dashboard-friends/friends-profile-view.tsx
@@ -0,0 +1,45 @@
+import { SwordsIcon, UserRoundMinusIcon } from "lucide-react"
+
+import type { FriendType } from "@/lib/common-types"
+
+import { Button } from "@/components/ui/button"
+import { UserItem } from "@/components/user-item"
+import UserStatistics from "@/components/user-statistics"
+import { formatDate } from "@/lib/utils"
+
+type PropsType = FriendType & {
+  handleInvite: () => void
+  handleUnfriend: () => void
+}
+
+export default function FriendsProfileView({
+  avatar,
+  handleInvite,
+  handleUnfriend,
+  signup_date,
+  stats,
+  status,
+  username,
+}: PropsType) {
+  return (
+    <>
+      <UserItem
+        avatar={avatar}
+        description={`Member since ${formatDate(signup_date)}`}
+        status={status}
+        title={username}
+      />
+      <UserStatistics {...stats} size="xs" />
+      <div className="flex gap-x-3">
+        <Button className="grow" onClick={handleUnfriend} variant="destructive">
+          <UserRoundMinusIcon />
+          <span>Unfriend</span>
+        </Button>
+        <Button className="grow" disabled={status !== "online"} onClick={handleInvite} variant="secondary">
+          <SwordsIcon />
+          <span>Invite to play</span>
+        </Button>
+      </div>
+    </>
+  )
+}
