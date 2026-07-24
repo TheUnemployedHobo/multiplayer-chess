7c8e7962096c42563f40be503d3a2263047f69bc
TheUnemplyedHobo <freakypumpkineer@gmail.com>
2026-07-24 18:45:48 +0330

    Refactor MatchFullHistory component to include search and filter functionality for match results

--- src/pages/dashboard/dashboard-match-history/match-full-history.tsx
+++ src/pages/dashboard/dashboard-match-history/match-full-history.tsx
@@ -1,4 +1,7 @@
 import { ScrollIcon } from "lucide-react"
+import { useState } from "react"
+
+import type { MatchHistoryType } from "@/lib/common-types"
 
 import { ShadcnDialog } from "@/components/shadcn-dialogs"
 import { Badge } from "@/components/ui/badge"
@@ -7,56 +10,77 @@ import { Input } from "@/components/ui/input"
 import { ItemGroup } from "@/components/ui/item"
 import { ScrollArea } from "@/components/ui/scroll-area"
 import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
-import { UserItem } from "@/components/user-item"
+import { UserItem, UserItemPlaceholder } from "@/components/user-item"
+import { cn, timeAgo } from "@/lib/utils"
 
-export default function MatchFullHistory() {
+type PropsType = { matches: MatchHistoryType[] | null | undefined }
+
+export default function MatchFullHistory({ matches }: PropsType) {
+  const [search, setSearch] = useState("")
+  const [result, setResult] = useState("no")
+
+  const filteredMatches = !matches
+    ? matches
+    : matches.filter(({ opponent, result: matchResult }) => {
+        const nameMatch = opponent.username.includes(search.toLowerCase())
+        const resultMatch = result === "no" ? true : matchResult === result
+        return nameMatch && resultMatch
+      })
+
   return (
     <ShadcnDialog
       content={
         <>
           <div className="flex gap-x-3">
-            <Input placeholder="Search for an opponent..." />
-            <Select defaultValue="wins">
+            <Input onChange={(e) => setSearch(e.target.value)} placeholder="Search for an opponent..." value={search} />
+            <Select onValueChange={setResult} value={result}>
               <SelectTrigger>
-                <SelectValue />
+                <SelectValue placeholder="Filter" />
               </SelectTrigger>
               <SelectContent>
                 <SelectGroup>
-                  <SelectItem value="wins">Wins</SelectItem>
-                  <SelectItem value="losses">Losses</SelectItem>
-                  <SelectItem value="draws">Draws</SelectItem>
+                  <SelectItem value="no">No filter</SelectItem>
+                  <SelectItem value="win">Wins</SelectItem>
+                  <SelectItem value="loss">Losses</SelectItem>
+                  <SelectItem value="draw">Draws</SelectItem>
                 </SelectGroup>
               </SelectContent>
             </Select>
           </div>
-          <ScrollArea>
+
+          <ScrollArea className="h-80">
             <ItemGroup>
-              <UserItem
-                actions={<Badge className="bg-red-700">Loss</Badge>}
-                avatar="e8wq4inw"
-                description="5 days ago"
-                title="Evil Rabbit"
-                variant="outline"
-              />
-              <UserItem
-                actions={<Badge className="bg-green-700">Win</Badge>}
-                avatar="e8wq4inw"
-                description="5 days ago"
-                title="Evil Rabbit"
-                variant="outline"
-              />
-              <UserItem
-                actions={<Badge className="bg-yellow-700">Draw</Badge>}
-                avatar="e8wq4inw"
-                description="5 days ago"
-                title="Evil Rabbit"
-                variant="outline"
-              />
+              {!matches ? (
+                <UserItemPlaceholder quantity={4} />
+              ) : !filteredMatches?.length ? (
+                <p className="text-muted-foreground">No recent matches.</p>
+              ) : (
+                filteredMatches.map(({ opponent, playedAt, result }) => (
+                  <UserItem
+                    actions={
+                      <Badge
+                        className={cn(
+                          result === "loss" && "bg-red-700",
+                          result === "draw" && "bg-amber-600",
+                          result === "win" && "bg-green-700",
+                        )}
+                      >
+                        {result}
+                      </Badge>
+                    }
+                    avatar={opponent.avatar}
+                    description={timeAgo(playedAt)}
+                    key={playedAt}
+                    title={opponent.username}
+                    variant="outline"
+                  />
+                ))
+              )}
             </ItemGroup>
           </ScrollArea>
         </>
       }
-      description="12 matches found"
+      description={`${filteredMatches?.length ?? 0} matches found`}
       title="All Matches"
       triggerButton={
         <Button variant="outline">
