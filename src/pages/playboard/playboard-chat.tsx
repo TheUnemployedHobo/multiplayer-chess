4c50133f4dd8b7ca1bbcffc1527363d68688eacb
TheUnemplyedHobo <freakypumpkineer@gmail.com>
2026-07-24 15:33:02 +0330

    Rename game chat hook for consistency

--- src/pages/playboard/playboard-chat.tsx
+++ src/pages/playboard/playboard-chat.tsx
@@ -7,13 +7,13 @@ import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/componen
 import { Input } from "@/components/ui/input"
 import { ScrollArea } from "@/components/ui/scroll-area"
 import useChessStore from "@/hooks/use-chess-store"
-import { useGameChat } from "@/lib/socket/use-game-events"
+import { useMpGameChat } from "@/lib/socket/use-game-events"
 
 export default function PlayBoardChat() {
   const [messages, setMessages] = useState<{ color: "black" | "white"; message: string }[]>([])
   const orientation = useChessStore((state) => state.orientation)
 
-  const sendMessage = useGameChat((message) => setMessages((p) => [...p, message]))
+  const sendMessage = useMpGameChat((message) => setMessages((p) => [...p, message]))
 
   return (
     <Card className="h-80 md:h-auto md:flex-1">
