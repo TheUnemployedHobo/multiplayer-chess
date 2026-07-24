480918400d8c5b05a35b709a0dc45af782bf63db
TheUnemplyedHobo <freakypumpkineer@gmail.com>
2026-07-24 18:45:18 +0330

    Refactor layout structure in DashboardPage for improved responsiveness

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -7,8 +7,8 @@ import DashboardUserStatistics from "./dashboard-user-statistics"
 
 export default function DashboardPage() {
   return (
-    <section className="container mx-auto flex h-dvh flex-col gap-3 p-3 lg:flex-row lg:items-center">
-      <main className="w-full min-w-0 space-y-3">
+    <section className="flex h-full flex-col gap-3 p-3 lg:max-h-250 lg:flex-row">
+      <main className="flex size-full flex-col gap-y-3">
         <DashboardProfile />
         <div className="flex flex-col gap-3 sm:flex-row">
           <DashboardMultiplayer />
